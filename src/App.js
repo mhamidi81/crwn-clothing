b673e387d3ea69bda4a955a861c10fe6c6d6bb7c
mohamed hamidi <mohamed.hamidi@gmail.com>
2019-08-19 18:43:44 +0100

    adding shop page, collection preview cpm & collection item cmp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
